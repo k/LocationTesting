47a73bbb29f7ed04b18fb391b62f9c02fddf300d
Kenneth Bambridge <k.bambridge@me.com>
2015-09-01 16:10:26 -0700

    Initial Commit

---
+++ LocationTesting/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LocationTesting
+//
+//  Created by Kenneth Bambridge on 9/1/15.
+//  Copyright © 2015 zoku. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
---
+++ LocationTestingTests/LocationTestingTests.swift
@@ -0,0 +1,36 @@
+//
+//  LocationTestingTests.swift
+//  LocationTestingTests
+//
+//  Created by Kenneth Bambridge on 9/1/15.
+//  Copyright © 2015 zoku. All rights reserved.
+//
+
+import XCTest
+@testable import LocationTesting
+
+class LocationTestingTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
